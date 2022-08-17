590b648aae042e38807c89cfa59b825194b8f829
Alexander Morales Garcia <Alex.morales1437@gmail.com>
2022-08-17 10:48:08 -0500

    deploy react to github pages

---
+++ src/resumeData.js
@@ -0,0 +1,119 @@
+let resumeData = {
+    "imagebaseurl":"https://github.com/amoralesg001",
+    "name": "Alexander Morales Garcia",
+    "role": "Software Engineer",
+    "linkedinId":"alex-morales-garcia-99b035154",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like to learn and create applications in various parts of software development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+    {
+    "name":"linkedin",
+   "url":"https://www.linkedin.com/in/alex-morales-garcia-99b035154/",
+    "className":"fa fa-linkedin"
+    },
+    {
+    "name":"github",
+    "url":"https://github.com/amoralesg001",
+    "className":"fa fa-github"
+    },
+    {
+    "name":"skype",
+    "url":"",
+    "className":"fa fa-twitter"
+    }
+    ],
+    "aboutme":"I am currently a Masters student at The Hochschule Fulda and pursuing my masters in Global software Development from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"Minnesota, United States",
+    "website":"https://ivy555.github.io",
+    "education":[
+    {
+    "UniversityName":"Saint John's University",
+    "specialization":"Computer Science Major with a Minor in Psychology",
+    "MonthOfPassing":"May",
+    "YearOfPassing":"2018 - 2022",
+    "Achievements":"Some Achievements"
+    }
+    ],
+    "work":[
+    {
+    "CompanyName":"Inspire Medical Systems",
+    "specialization":"Software Engineer Intern",
+    "MonthOfLeaving":"June - August",
+    "YearOfLeaving":"2022",
+    "Achievements":"Some Achievements"
+    },
+    {
+    "CompanyName":"IT Services",
+    "specialization":"Help Desk Assistant",
+    "MonthOfLeaving":"May",
+    "YearOfLeaving":"2018 - 2022",
+    "Achievements":"Some Achievements"
+    },
+    {
+    "CompanyName":"Penn State Applied Research Laboratory",
+    "specialization":"User Experience Intern",
+    "MonthOfLeaving":"June - August",
+    "YearOfLeaving":"2021",
+    "Achievements":"Some Achievements"
+    },
+    {
+    "CompanyName":"Applied Social/Cognitive Research Assistant ",
+    "specialization":"Some specialization",
+    "MonthOfLeaving":"August",
+    "YearOfLeaving":"2020 - 2021",
+    "Achievements":"Some Achievements"
+    },
+    {
+    "CompanyName":"Virtual Nursing Simulation Developer",
+    "specialization":"Freelance",
+    "MonthOfLeaving":"December",
+    "YearOfLeaving":"2020",
+    "Achievements":"Some Achievements"
+    }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+    {
+    "skillname":"HTML5"
+    },
+    {
+    "skillname":"CSS"
+    },
+    {
+    "skillname":"Reactjs"
+    }
+    ],
+    "portfolio":[
+    {
+    "name":"project1",
+    "description":"mobileapp",
+    "imgurl":"images/portfolio/phone.jpg"
+    },
+    {
+    "name":"project2",
+    "description":"mobileapp",
+    "imgurl":"images/portfolio/project.jpg"
+    },
+    {
+    "name":"project3",
+    "description":"mobileapp",
+    "imgurl":"images/portfolio/project2.png"
+    },
+    {
+    "name":"project4",
+    "description":"mobileapp",
+    "imgurl":"images/portfolio/phone.jpg"
+    }
+    ],
+    "testimonials":[
+    {
+    "description":"This is a sample testimonial",
+    "name":"Some technical guy"
+    },
+    {
+    "description":"This is a sample testimonial",
+    "name":"Some technical guy"
+    }
+    ]
+    }
+    export default resumeData
